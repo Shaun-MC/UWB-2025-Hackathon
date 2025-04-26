6c50b8b2deefc50b952f375a887c58519e0b3812
Iliya Belyak-laptop <iliyabel2003@outlook.com>
2025-04-25 20:06:58 -0700

    Added a lil bit of html and css.

--- Hackathon2025UWB/src/App.jsx
+++ Hackathon2025UWB/src/App.jsx
@@ -5,7 +5,27 @@ function App() {
 
   return (
     <>
-      <TweetDisplay id="1915450756615831969"/>
+      <header className="app-header">
+        <span>
+          {/* This is a temp logo */}
+          <img src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Firefox_logo%2C_2019.svg" alt="Test logo" class="logo" />
+          <h1>Watch.global</h1>
+        </span>
+      </header>
+
+      <nav>
+      <ul className='navbar'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Our team</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      </nav>
+
+
+      <div className="main-containter">
+        <TweetDisplay id="1915450756615831969"/>
+      </div>
     </>
   )
 }
