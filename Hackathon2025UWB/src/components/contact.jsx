cf546204b1f09c2b523a164661216564a78132cd
Karsten Schmidt <kbskjs1@gmail.com>
2025-04-27 09:55:35 -0700

    finalized

---
+++ Hackathon2025UWB/src/components/contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function contact() {
+  return (
+    <>
+    <div className="team-header" style={{paddingTop: "50px"}}>
+        <img src="/watch_global_finals-01.svg" alt="Watch.global Logo" className="logo" />
+        <h1>Watch.global</h1>
+    </div>
+    <div style={{textAlign: "center"}}>
+        <p>We are currently reachable by contacting us at our UW emails:</p>
+        <p>
+            kars10@uw.edu<br />
+            moumin@uw.edu<br />
+            iliyabel@uwb.edu<br />
+            scushm@uw.edu
+        </p>
+    </div>
+    </>
+  )
+}
---
+++ Hackathon2025UWB/src/components/projects.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function projects() {
+  return (
+    <>
+    <div className="team-header" style={{paddingTop: "50px"}}>
+        <img src="/watch_global_finals-01.svg" alt="Watch.global Logo" className="logo" />
+        <h1>Watch.global</h1>
+    </div>
+<div style={{textAlign: "center"}}>
+    <h2> <strong> Our Projects!</strong></h2>
+    <h3>Watch Global</h3>
+    <p>We are currently working on a project that aims to create a crisis map that will help people in need.</p>
+ </div>
+ 
+</>
+  )
+}
