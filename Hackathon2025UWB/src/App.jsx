12d211e538b362e568a0ee11a18b4e26c0cc5d33
Shaun-MC <shaunmcushman@gmail.com>
2025-04-26 20:04:25 -0700

    nit picky changes

--- Hackathon2025UWB/src/App.jsx
+++ Hackathon2025UWB/src/App.jsx
@@ -31,13 +31,13 @@ function App() {
         <span>
           {/* This is a temp logo */}
           <img src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Firefox_logo%2C_2019.svg" alt="Test logo" className="logo" />
-          <h1>Watch.global</h1>
+          <h1>watch.global</h1>
         </span>
 
       <nav>
       <ul className='navbar'>
         <li><a href="#">Home</a></li>
-        <li><a href="#">Our team</a></li>
+        <li><a href="#">Our Team</a></li>
         <li><a href="#">Projects</a></li>
         <li><a href="#">Contact</a></li>
       </ul>
@@ -48,17 +48,6 @@ function App() {
         
         <div className="crisis-map">
           <h2>Crisis Map</h2>
-          {/* API_KEY NEEDED */} 
-          {/* <iframe 
-            src="https://www.google.com/maps/embed/v1/place?key="
-            width="70%" 
-            height="600px" 
-            style="border:0"
-            loading="lazy"
-            allowfullscreen
-            referrerpolicy="no-referrer-when-downgrade"
-            title="Crisis Map">
-          </iframe> */}
         </div>
         {fetchedCrisis.map((tweet) => (
           <TweetDisplay key={tweet.postid} id={tweet.postid} />
